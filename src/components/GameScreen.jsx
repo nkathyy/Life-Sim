5c9ddb21947926ef001411ab8af4be2ba2cbe472
Kathy <kathyaudrey2001@gmail.com>
2025-08-25 20:25:45 +0800

    include out section in game section

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,12 +1,28 @@
 import EventCard from "./EventCard";
 import "./GameScreen.css";
+import OutWindow from "./OutWindow";
+import { useState } from "react";
 
 const GameScreen = () => {
+  const [isOutWindowOpen, setIsOutWindowOpen] = useState(true);
+
+  const openOutWindow = () => {
+    setIsOutWindowOpen(true);
+  };
+
+  const closeOutWindow = () => {
+    setIsOutWindowOpen(false);
+  };
+
   return (
     <>
+      <OutWindow
+        isOutWindowOpen={isOutWindowOpen}
+        closeWindow={closeOutWindow}
+      />
       <div className="charContainer"></div>
       <div className="eventContainer">
-        <EventCard title={"Sample"} />
+        <EventCard title={"Sample"} onClick={openOutWindow} />
         <EventCard title={"例子"} />
         <EventCard title={"例子"} />
         <EventCard title={"Sample"} />
