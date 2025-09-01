be04b0d6d586f0d14bed8c8003c131a4280950b2
Kathy <kathyaudrey2001@gmail.com>
2025-09-01 13:40:38 +0800

    include date data

---
+++ src/constants/dateData.js
@@ -0,0 +1,117 @@
+export const dailyDateData = [
+  {
+    index: 0,
+    dataName: "happiness",
+    result: (num) => `added ${num} 1`,
+  },
+  {
+    index: 1,
+    dataName: "happiness",
+    result: (num) => `added ${num} 2`,
+  },
+  {
+    index: 2,
+    dataName: "happiness",
+    result: (num) => `added ${num} 3`,
+  },
+  {
+    index: 3,
+    dataName: "happiness",
+    result: (num) => `added ${num} 4`,
+  },
+  {
+    index: 4,
+    dataName: "happiness",
+    result: (num) => `added ${num} 5`,
+  },
+  {
+    index: 5,
+    dataName: "happiness",
+    result: (num) => `added ${num} 6`,
+  },
+  {
+    index: 6,
+    dataName: "happiness",
+    result: (num) => `added ${num} 7`,
+  },
+  {
+    index: 7,
+    dataName: "happiness",
+    result: (num) => `added ${num} 8`,
+  },
+];
+
+export const weeklyDate = [
+  {
+    index: 0,
+    dateDescription: "xxx1 invitation!",
+    choices: [
+      {
+        index: 0,
+        choiceName: "choice 1", //button display name
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+      {
+        index: 1,
+        choiceName: "choice 2",
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+    ],
+  },
+  {
+    index: 1,
+    dateDescription: "xxx2 invitation!",
+    choices: [
+      {
+        index: 0,
+        choiceName: "choice 3", //button display name
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+      {
+        index: 1,
+        choiceName: "choice 4",
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+    ],
+  },
+  {
+    index: 2,
+    dateDescription: "xxx3 invitation!",
+    choices: [
+      {
+        index: 0,
+        choiceName: "choice 5", //button display name
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+      {
+        index: 1,
+        choiceName: "choice 6",
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+    ],
+  },
+  {
+    index: 3,
+    dateDescription: "xxx4 invitation!",
+    choices: [
+      {
+        index: 0,
+        choiceName: "choice 7", //button display name
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+      {
+        index: 1,
+        choiceName: "choice 8",
+        dataName: "",
+        result: (num) => `added ${num}`,
+      },
+    ],
+  },
+];
